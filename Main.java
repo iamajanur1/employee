f2c95531696b61d7fcdf9bba697e2177022ed406
iamajanur1 <70979095+iamajanur1@users.noreply.github.com>
2020-10-10 13:22:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        employee e = new employee();
+
+        //    employee.data=55;
+
+
+        e.dosomething();
+        e.donothing();
+
+
+    }
+}
---
+++ employee.java
@@ -0,0 +1,28 @@
+package com.employee;
+
+public class employee {
+
+   int age=20;
+     String name=new String("AJANUR");
+     String city=new String("GUWAHATI");
+
+
+  public void dosomething(){
+
+
+   System.out.println(name);
+      System.out.println(city);
+      System.out.println(age);
+
+    }
+    public void donothing(){
+
+
+        System.out.println(name);
+        System.out.println(city);
+        System.out.println(age);
+
+    }
+    
+
+}
